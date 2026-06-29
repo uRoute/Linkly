985eb786f0d9891062d5df2ee051950e344f4da2
Youssef samir <usef.route@gmail.com>
2026-06-29 20:20:17 +0300

    .

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { AuthLayoutComponent } from './core/layouts/components/auth-layout/auth-layout.component';
+import { LoginComponent } from './features/components/login/login.component';
+import { RegisterComponent } from './features/components/register/register.component';
+import { MainLayoutComponent } from './core/layouts/components/main-layout/main-layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'',redirectTo:'login',pathMatch:'full'},
+    {path:'' , component:AuthLayoutComponent,children:[
+        {path:'login',component:LoginComponent,title:'Linkly | Login'},
+        {path:'register',component:RegisterComponent,title:'Linkly | Register'}
+    ]},
+    {path:'' , component:MainLayoutComponent,children:[
+        {path:'home',loadComponent:()=> import('./features/components/home/home.component').then((c)=>c.HomeComponent) ,title:'Linkly | Home'},
+        {path:'uer_profile',loadComponent:()=> import('./features/components/profile/profile.component').then((c)=>c.ProfileComponent) ,title:'Linkly | Profile'},
+        // {path:'home',loadComponent:()=> import('./features/components/').then((c)=>c.HomeComponent) ,title:'Linkly | Home'},
+        // {path:'home',loadComponent:()=> import('./features/components/home/home.component').then((c)=>c.HomeComponent) ,title:'Linkly | Home'},
+
+    ]},
+    {path:'**' , loadComponent: ()=> import('./shared/components/wildcard/wildcard.component').then((c)=> c.WildcardComponent) , title:'Linkly | 404' }
+];
--- src/app/core/layouts/components/auth-layout/auth-layout.component.ts
+++ src/app/core/layouts/components/auth-layout/auth-layout.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from "../../../../shared/components/navbar/navbar.component";
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-auth-layout',
-  imports: [],
+  imports: [NavbarComponent, RouterOutlet],
   templateUrl: './auth-layout.component.html',
   styleUrl: './auth-layout.component.scss'
 })

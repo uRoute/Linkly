9b441195e3b72e99de056367fb97b151305f5dec
Youssef samir <usef.route@gmail.com>
2026-06-30 19:33:10 +0300

    //

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,11 +3,14 @@ import { provideRouter, withViewTransitions } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideToastr } from 'ngx-toastr';
+
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes , withViewTransitions()),
     provideHttpClient(withFetch()),
     importProvidersFrom(),
+    provideToastr(),
     provideClientHydration(withEventReplay())
   ]
 };
--- src/app/features/components/login/login.component.ts
+++ src/app/features/components/login/login.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms'
+import { Component, inject } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms'
+import { Router } from '@angular/router';
+import { AuthenticationService } from '../../../shared/services/authentication/authentication.service';
+import { ToastrService } from 'ngx-toastr';
 @Component({
   selector: 'app-login',
   imports: [ReactiveFormsModule],
@@ -7,5 +10,28 @@ import { ReactiveFormsModule } from '@angular/forms'
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+
+  private _Router = inject(Router)
+  private _AuthenticationService = inject(AuthenticationService)
+  private _ToastrService = inject(ToastrService)
+  isSpinner:boolean = false
+
+  loginForm:FormGroup = new FormGroup({
+    email: new FormControl(null , [Validators.required,Validators.email]),
+    password : new FormControl(null , [Validators.required , Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*(),.?":{}|<>]).{8,}$/)])
+  })
+
+  Login(){
+    if(this.loginForm.valid){
+      this.isSpinner=true;
+      this._AuthenticationService.SignIn(this.loginForm.value).subscribe({
+        next:(res)=>{
+          console.log(res);
+          this._ToastrService.info('Hello')
+        }
+      })
+    }
+  }
+
 
 }
